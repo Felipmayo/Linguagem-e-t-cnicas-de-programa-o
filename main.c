20978dd39854ec2401e27ca073619ffa01357910
Felipmayo <felipedemoraismayo@gmail.com>
2026-08-06 11:38:53 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+    float a,b,c,d,e,f;
+	
+    a = 8;
+    b = 19;
+    c = a/b;
+    d = a+b;
+    e = a-b;
+    f = a*b;
+    
+	printf("a divisao de %f / %f = %f",a,b,c);
+	printf("a soma de %f + %f = %f",a,b,d);
+	printf("a subtraçao de %f - %f = %f",a,b,e);
+	printf("a multiplicacao de %f * %f = %f",a,b,f);
+	
+	return 0;
+}
