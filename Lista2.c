df8692d042491946611469d356ce803a1f378296
Felipmayo <felipedemoraismayo@gmail.com>
2026-08-18 17:35:51 -0300

    Create Lista2.c

---
+++ Lista2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+/* 1) */
+
+   int idade, ano;
+   printf("Quantos anos voce tem? ");
+   scanf("%d", &idade);
+   printf("Insira o ano atual: ");
+   scanf("%d", &ano);
+   printf("Voce tem %d anos e nasceu em %d", idade, ano - idade);
+
+/* 2) */
+
+   float k, m;
+   printf("\nInsira a velocidade em km/h: ");
+   scanf("%f", &k);
+   m = k / 3.6;
+   printf("%f km/h convertido para metros por segundo fica: %f m/s", k, m);
+
+/* 3) */
+
+   float reais, dolar, total;
+   printf("\nInsira o valor em reais: ");
+   scanf("%f", &reais);
+   printf("Insira cotacao do dolar: ");
+   scanf("%f", &dolar);
+   total = reais / dolar;
+   printf("%.2f reais em dolar vale: %.2f dolares", reais, total);
+
+/* 4) */
+
+
+  return 0;
+}
