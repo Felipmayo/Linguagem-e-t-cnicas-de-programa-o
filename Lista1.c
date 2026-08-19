46f490e6053a7607024c92c32ff63b463b975a66
Felipmayo <felipedemoraismayo@gmail.com>
2026-08-19 11:07:55 -0300

    Update Lista1.c

--- Lista1.c
+++ Lista1.c
@@ -133,9 +133,9 @@ double x1, y1, x2, y2, d;
    printf("Insira o y2: ");
    scanf("%lf", &y2);
 
-   d = sqrt((x2 - x1) * (x2 - x1)+
-            (y2 - y1) * (y2 - y1));
-   printf("A distancia euclidiana vale: %.5lf", d);
+   d = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+   printf("A distancia euclidiana vale: %lf", d);
 	
 	return 0;
 }
