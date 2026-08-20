535c5c066e296b95fdebbcd492f8da0408e6eddb
Felipmayo <felipedemoraismayo@gmail.com>
2026-08-20 16:13:34 -0300

    Refactor variable declarations and print statements

--- Lista1.c
+++ Lista1.c
@@ -32,8 +32,8 @@ int main(int argc, char *argv[]) {
 /* 3) Implemente um programa que leia um número n [com n positivo & n <= 64] e mostre na tela o seu valor em base binária (bits). */
 
     int bit1, bit2, bit3, bit4, bit5, bit6, bit7, bitx, n;
-      printf("\nInsira o valor para ser convertido: ");
-      scanf("%d", &bitx);
+    printf("\nInsira o valor para ser convertido: ");
+    scanf("%d", &bitx);
 	
      n = bitx;
 	
@@ -64,20 +64,20 @@ int main(int argc, char *argv[]) {
 Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, mostre o total a receber no final do mês, 
 com duas casas decimais. */
 
-float salario, vendas, total;
+   float salario, vendas, total;
    printf("\nInsira o salario: ");
    scanf("%f", &salario);
 
    printf("Insira as vendas efetuadas: ");
    scanf("%f", &vendas);
 
- total = (vendas * 0.15) + salario;
+   total = (vendas * 0.15) + salario;
 
    printf("O total a ser recebido: %.2f", total);
 
 /* 5) Elabore um programa que peça ao usuário para digitar 4 valores. E mostre na tela a soma, a média e o produtório desses valores. */
 
-float num1, num2, num3, num4;
+   float num1, num2, num3, num4;
    printf("\nInsira o primeiro numero: ");
    scanf("%f", &num1);
 
@@ -98,32 +98,32 @@ float num1, num2, num3, num4;
 
 /* 6) Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias. */
 
-int diasvividos, anos, meses, dias;
-  printf("\nInsira quantos dias voce tem de vida: ");
-  scanf("%d", &diasvividos);
+   int diasvividos, anos, meses, dias;
+   printf("\nInsira quantos dias voce tem de vida: ");
+   scanf("%d", &diasvividos);
 
-  anos = diasvividos / 365;
-  diasvividos = diasvividos %365;
-  meses = diasvividos / 30;
-  diasvividos = diasvividos %30;
+   anos = diasvividos / 365;
+   diasvividos = diasvividos %365;
+   meses = diasvividos / 30;
+   diasvividos = diasvividos %30;
 
-  printf("voce tem: %d anos, %d meses e %d dias de vida", anos, meses, diasvividos);
+   printf("voce tem: %d anos, %d meses e %d dias de vida", anos, meses, diasvividos);
 
 /* 7) Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R).
 A fórmula para calcular o volume é: (4/3) * pi * R³. Considere (atribua) para pi o valor 3.14159. */
 
-float r, volume, pi;
-  pi = 3.14159;
-  printf("\nInsira o valor do raio da esfera: ");
-  scanf("%f", &r);
-
-  volume = (4 / 3.0) * pi * r * r * r;
-  printf("O volume da esfera vale: %f", volume);
+   float r, volume, pi;
+   pi = 3.14159;
+   printf("\nInsira o valor do raio da esfera: ");
+   scanf("%f", &r);
+	
+   volume = (4 / 3.0) * pi * r * r * r;
+   printf("O volume da esfera vale: %f", volume);
 
 /* 8) Leia os quatro valores do usuário correspondentes às coordenadas em um plano cartesiano, p1(x1,y1) e p2(x2,y2)
 e calcule e mostre a distância entre eles, aplicando a distância euclidiana, segundo a fórmula: d = √((x₂ − x₁)² + (y₂ − y₁)²) */
 
-double x1, y1, x2, y2, d;
+   double x1, y1, x2, y2, d;
    printf("\nInsira o x1: ");
    scanf("%lf", &x1);
 	
