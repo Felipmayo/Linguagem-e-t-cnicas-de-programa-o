4c3fe5333f5fe990b208e89ba06139354a290cde
Felipmayo <felipedemoraismayo@gmail.com>
2026-08-20 11:16:23 -0300

    Update Lista2.c

--- Lista2.c
+++ Lista2.c
@@ -52,5 +52,38 @@ int main(int argc, char *argv[]) {
 
    printf("O angulo de %f graus vale %f em radianos", G, R);
 
+   /* 6) */
+
+   int inteiro;
+   printf("\nInsira o numero: ");
+   scanf("%d", &inteiro);
+  
+   printf("O sucessor de %d vale: %d", inteiro, inteiro + 1);
+   printf("\nO antecessor de %d vale: %d", inteiro, inteiro - 1);
+
+
+   /* 10.1 */
+
+   int a, b, c, maior, maior_temp;
+   printf("\nInforme os valores a serem comparados: ");
+   scanf("%d %d %d", &a, &b, &c);
+
+   //conta
+   maior_temp = (a+b+abs(a-b))/2;
+   maior = (maior_temp+c+abs(maior_temp-c))/2;
+   printf("O maior entre |%d||%d||%d| = %d", a, b, c, maior);
+
+   /* 10.2 */
+
+   int a1, b1, c1, d1, maior_temp2, maior_temp1, maior1;
+   printf("\nInforme os valores a serem comparados: ");
+   scanf("%d %d %d %d", &a1, &b1, &c1, &d1);
+
+   //conta 2
+   maior_temp1 = (a1+b1+abs(a1-b1))/2;
+   maior_temp2 = (maior_temp1+c1+abs(maior_temp1-c1))/2;
+   maior1 = (maior_temp2+d1+abs(maior_temp2-d1))/2;
+   printf("O maior entre |%d||%d||%d||%d| = %d", a1, b1, c1, d1, maior1);
+
   return 0;
 }
