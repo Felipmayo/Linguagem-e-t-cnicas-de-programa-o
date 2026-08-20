4f04a3fec24d75c18a5739b4039db05a424fefd3
Felipmayo <felipedemoraismayo@gmail.com>
2026-08-20 15:43:19 -0300

    Add calculations for time duration and travel distance

--- Lista2.c
+++ Lista2.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[]) {
    scanf("%d", &idade);
    printf("Insira o ano atual: ");
    scanf("%d", &ano);
+   
    printf("Voce tem %d anos e nasceu em %d", idade, ano - idade);
 
 /* 2) */
@@ -17,6 +18,7 @@ int main(int argc, char *argv[]) {
    float k, m;
    printf("\nInsira a velocidade em km/h: ");
    scanf("%f", &k);
+   
    m = k / 3.6;
    printf("%f km/h convertido para metros por segundo fica: %f m/s", k, m);
 
@@ -27,6 +29,7 @@ int main(int argc, char *argv[]) {
    scanf("%f", &reais);
    printf("Insira cotacao do dolar: ");
    scanf("%f", &dolar);
+   
    total = reais / dolar;
    printf("%.2f reais em dolar vale: %.2f dolares", reais, total);
 
@@ -64,7 +67,6 @@ int main(int argc, char *argv[]) {
    /* 7) */
 
    float ganhador, segundo, terceiro;
-
    ganhador = 780000*0.46;
    segundo = 780000*0.32;
    terceiro = 780000 - (ganhador + segundo);
@@ -74,6 +76,29 @@ int main(int argc, char *argv[]) {
    printf("\nO terceiro ganhou: %.2f reais", terceiro);
 
    /* 8) */
+
+   int horas, minutos, segundos;
+   printf("\nInsira o tempo de duração em segundos: ");
+   scanf("%d", &segundos);
+
+   horas = segundos / 3600;
+   segundos = segundos % 3600;
+   minutos = segundos / 60;
+   segundos = segundos % 60;
+
+   printf("O evento durou: %dh %dm %ds", horas, minutos, segundos);
+
+   /* 9) */
+
+   float velocidade, duracao, gasolina, distancia;
+   printf("Insira quato tempo durou a viagem em horas: ");
+   scanf("%f", &duracao);
+   printf("Insira a velocidade media durante a viagem em Km/h: ");
+   scanf("%f", &velocidade);
+   
+   distancia = velocidade * duracao;
+   gasolina = distancia / 12;
+   printf("Para percorrer a distancia de %.2f Km, vai precisar de %.2f litros de gasolina", distancia, gasolina);
 
    
 
