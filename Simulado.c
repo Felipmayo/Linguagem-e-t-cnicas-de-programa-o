8b865c44989bab19a27d0c4493f6951bdd098755
Felipmayo <felipedemoraismayo@gmail.com>
2026-09-15 17:07:07 -0300

    Update Simulado.c

--- Simulado.c
+++ Simulado.c
@@ -15,3 +15,49 @@ int main(){
 
   printf("%d %d %d %d", A, B, C, D);
 }
+
+float calc_vpa(float valor_patrimonial, float quantidade_acoes){
+    return valor_patrimonial / quantidade_acoes;
+}
+float calc_pvp(float preco_atual_acao, float vpa){
+    return preco_atual_acao / vpa;
+}
+
+
+int main(){
+    float valor_patrimonial, quantidade_acoes, preco_atual_acao, pvp, vpa;
+    
+    printf("Insira o valor patrimonial da empresa: ");
+    scanf("%f", &valor_patrimonial);
+    printf("Insira a quantidade de acoes disponiveis: ");
+    scanf("%f", &quantidade_acoes);
+    printf("Insira o preco atual da acao: ");
+    scanf("%f", &preco_atual_acao);
+
+    vpa = calc_vpa(valor_patrimonial, quantidade_acoes);
+    pvp = calc_pvp(preco_atual_acao, vpa);
+
+    printf("============================\n");
+    printf("VPA:    %f\n", vpa);
+    printf("============================\n");
+    printf("P/VP:   %f\n", pvp);
+    printf("============================\n");
+    if(pvp < 0){
+        printf("Class: Pessima!");
+    }
+    else if(pvp >= 0 && pvp < 0.8){
+        printf("Class: Otima!");
+    }
+    else if(pvp >= 0.8 && pvp <= 1.2){
+        printf("Class: Indiferente!");
+    }
+    else if(pvp > 1.2 && pvp <= 2.0){
+        printf("Class: Boa!");
+    }
+    else{
+        printf("Class: Ruim!");
+    }
+
+return 0;
+
+}
